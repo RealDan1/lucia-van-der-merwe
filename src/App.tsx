8f58e4c3ab75eacdddc2c8823b57d5a3fd8aeee1
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-05-02 10:37:42 +0200

    feat: scaffold react router for tabs system(WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import Home from './components/Home';
-
+import MainLayout from './components/MainLayout';
 import './App.css';
-
+import { Routes, Route, Link } from 'react-router';
 function App() {
   return (
     <>
-      <Home></Home>
+      <nav>
+        <p>this is the nav section</p>
+        <div className="links">
+          <Link to="/">Home</Link>
+        </div>
+        <hr />
+      </nav>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route index path="/" element={Home} />
+        </Route>
+      </Routes>
     </>
   );
 }
---
+++ src/components/MainLayout.tsx
@@ -0,0 +1,10 @@
+const MainLayout = () => {
+  <div>
+    <h1>Welcome to the Home Page</h1>
+    <footer>
+      <hr />
+      <p>this is the footer</p>
+    </footer>
+  </div>;
+};
+export default MainLayout;
